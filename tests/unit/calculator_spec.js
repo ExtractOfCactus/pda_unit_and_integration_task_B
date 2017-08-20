7828f432f331c6ab357fb03e812f379a96c5d26f
Glen Young <extractofcactus@icloud.com>
2017-08-20 09:09:36 +0100

    add; subtract; multiply; divide passed

---
+++ tests/unit/calculator_spec.js
@@ -0,0 +1,47 @@
+var Calculator = require('../../public/js/calculator.js')
+var assert = require('assert')
+
+describe('calculator', function () {
+  beforeEach(function () {
+    calculator = new Calculator()
+  });
+
+  // write unit tests here in the form of "it should do something..."
+  it('it has a sample test', function(){
+    assert.equal(true, true)
+  })
+
+  it('has a runningTotal that starts at 0', function() {
+    assert.equal(calculator.runningTotal, 0);
+  });
+
+  it('can add two numbers', function() {
+    calculator.previousTotal = 2;
+    calculator.add(3);
+    assert.equal(calculator.runningTotal, 5);
+  });
+
+  it('can subtract two numbers', function() {
+    calculator.previousTotal = 15;
+    calculator.subtract(8);
+    assert.equal(calculator.runningTotal, 7);
+  });
+
+  it('can multiply two numbers', function() {
+    calculator.previousTotal = 6;
+    calculator.multiply(5); 
+    assert.equal(calculator.runningTotal, 30);
+  });
+
+  it('can divide two numbers', function() {
+    calculator.previousTotal = 24;
+    calculator.divide(4);
+    assert.equal(calculator.runningTotal, 6);
+  });
+
+  it('can have a number clicked', function() {
+    git remote add origin git@github.com:ExtractOfCactus/Unit_and_Integration_Task_B.git
+    git push -u origin master
+  })
+
+});
